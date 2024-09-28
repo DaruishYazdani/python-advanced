50451f3415cb5186424a9cad0f5dc6f8a654eb93
DaruishYazdani <alirezayazdani1304@gmail.com>
2024-09-28 16:21:49 +0330

    Delete fibonachi.py

--- fibonachi.py
+++
@@ -1,8 +0,0 @@
-n = int(input("how many? "))
-fibo_list = []
-for i in range(n):
-    if i == 0 or i == 1:
-        fibo_list.append(1)
-    else:
-        fibo_list.append(fibo_list[i-1] + fibo_list[i-2])
-print(fibo_list)
