1c645debc13d06b057e9bb86d05660f065791252
Daruishyazdani <alirezayazdani1304@gmail.com>
2024-10-08 21:52:59 +0330

    tamrin tuple

--- jalase_4/tupel.py
+++ jalase_4/tupel.py
@@ -1,1 +1,9 @@
-print("I dont know")
+
+a = int(input("Enter the number tuples: "))
+tuples = []
+for i in range (a):
+            b= input(f"{i+1}tuple seperated by a space (a b): ")
+            s , y = map(int, b.split())
+            tuples.append((s, y))
+c = sorted(tuples, key=lambda x: x [1])
+print(c)
