d769b900d3eb710a6b14e9988c061156931d4d43
Daruishyazdani <alirezayazdani1304@gmail.com>
2024-10-18 21:32:09 +0330

    tamrin11

--- jalase_7/11_text.py
+++ jalase_7/11_text.py
@@ -1,24 +1,21 @@
-a = input("enter book:")
-
-with open(a,"r" , encoding= "utf-8") as file :
-    b = file.read()
-
-def book (words):
-    word = 0
-    say_the_word = words.split()
-    for i in say_the_word:
-        word =+ 1
-        return word
-def books (search,word):
-    words = 0
-    say_the_word = search.split()
-    for j in word:
-        say_the_word =words.count(word)
-        return say_the_word
-search_word = input("kalame morede nazar ra vared konid : ")
-say_the_word = search_word.split()
-with open("report.text", "w", encoding="utf-8") as report:
-    report.write(f"The count of the word you gave me: {search_word}""\n")
-    report.write(f"The count of all words: {say_the_word}""\n")
-with open("report.txt", "r", encoding='utf-8') as reportt:
-    print(reportt.read())
+book = input("lotfan fail ra vared konid:")
+with open (book,"r",encoding="utf_8") as file:
+    count = file.read()
+p = (len(count))
+def kh (khat):
+    count = 0
+    for i in khat:
+       if i == " ":
+            count = count+1
+word = input("kalame mored nazar :")
+massage = book
+v = (massage.find(word))
+with open ("report.text","w",encoding="utf_8") as report:
+    report.write("/n"f"tedat kalamat hast : {p}""/n")
+    report.write(f"tedad khat ha hast: {count}""/n")
+    report.write(f"tedad kalame mored nazar : {v}""/n")
+with open ("report.text","r",encoding="utf_8") as report:
+    report=report.read()
+    print(report)
+with open ("report.text","w",encoding="utf_8") as report:
+    report.write(" ")
